43aac9b394415355c8afe434584c1626e301e211
sebastian villarroel <sebastianalvarez12121@gmail.com>
2026-03-09 15:35:19 -0500

    Delete conversor de monedas directory

--- conversor de monedas/src/conversormoneda.java
+++
@@ -1,5 +0,0 @@
-public class ConversorMoneda {
-    public double convertir(double monto, double tasaConversion) {
-        return monto * tasaConversion;
-    }
-}
--- conversor de monedas/src/moneda.java
+++
@@ -1,5 +0,0 @@
-public record Moneda(
-        String base_code,
-        String target_code,
-        double conversion_rate) {
-}
